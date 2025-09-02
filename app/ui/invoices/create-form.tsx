192d3d57765da9def0b3e4204cc717b66be6c161
Your Name <felipelimamiranda66@gmail.com>
2025-09-02 17:53:41 -0300

    Fixing state

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { useActionState } from 'react';
-import { createInvoice, State } from '@/app/lib/actions';
+import { createInvoice} from '@/app/lib/actions';
+import { State } from '@/app/lib/definitions';
 
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
@@ -12,6 +13,7 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { StatementSync } from 'node:sqlite';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState: State = { message: null, errors: {} };
